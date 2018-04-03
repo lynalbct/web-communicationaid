2532541eabbc294a21a5b42141217e8003341360
ruelbontuyan <ruelbontuyan@yahoo.com>
2018-04-04 04:03:27 +0800

    update enroll

--- app.py
+++ app.py
@@ -6,90 +6,35 @@ from model import Class, Child, Account, Teacher, db
 import json
 import os
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:regards@localhost/db'
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/db'
+app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-def spcall(qry, param, commit=False):
-    try:
-        dbo = model.DBconn()
-        cursor = dbo.getcursor()
-        cursor.callproc(qry, param)
-        res = cursor.fetchall()
-        if commit:
-            dbo.dbcommit()
-        return res
-    except:
-        res = [("Error: " + str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]),)]
-    return res
 
 
-# @app.route('/addstudent/', methods=['POST'])
-# def students():
-
-#     params = request.get_json()
-#     c_id = params["childid"]
-
-
-#     res = spcall('students', (childid), True)
-#     if 'Error' in res[0][0]:
-#         return jsonify({'status': 'error', 'message': res[0][0]})
-
-#     return jsonify({'status': 'ok', 'message': res[0][0]})
-
-# @app.route('/teacher_view', methods=['GET'])
-# def studentsview():
-#      res = spcall('studentsview', (), True)
-
-#      if 'Error' in str(res[0][0]):
-#          return jsonify({'status': 'error', 'message': res[0][0]})
-
-#      recs = []
-#      for r in res:
-#       recs.append({"teacher": str(r[0]), "fname": str(r[1]), "lname": str(r[2])})
-#      return jsonify({'status': 'ok', 'entries': recs, 'count': len(recs)})
-
-
-# @app.route('/teacher_edit', methods=['POST'])
-# def editstudents():
-
-#     params = request.get_json()
-#     fname = params["fname"]
-#     mname = params["mname"]
-#     lname = params["lname"]
-#     bday  = params["bday"]
-#     Class = params["Class"]
-
-
-#     res = spcall('editstudents', (fname, mname, lname, bday, Class), True)
-
-#     if 'Error' in res[0][0]:
-#         return jsonify({'status': 'error', 'message': res[0][0]})
-#     return jsonify({'status': 'ok', 'message': res[0][0]})
-@app.route('/Class', methods=['GET', 'POST'])
+@app.route('/Class')
 def Class():
-    db.create_all()
-    result1 = Class.query.order_by(Class.class_id)
-    return render_template('class.html', Class=result1, )
+    result1 = Class.query.order_by(Class.class_id).all()
+    return render_template('class.html', result1=result1)
 
 
 @app.route('/addClass', methods=['POST', 'GET'])
 def addClass():
     if request.method == 'POST':
-        Class = Class(request.form['c_id'])
+        class_name = request.form['class_name']
+        Class = Class(class_name=class_name)
         db.session.add(Class)
         db.session.commit()
         return redirect(url_for('class'))
     else:
-        return render_template('addclass.html', Class=Class)
-    # else:
-    #     return render_template('addclass.html', Class=Class)
+        return render_template('addclass.html')
 
 
-@app.route('/deleteClass<class_id>', methods=['GET', 'POST'])
+@app.route('/deleteClass/<class_id>', methods=['GET', 'POST'])
 def deleteClass():
     Class = Class.query.order_by(Class.class_id)
     if request.method == 'POST':
@@ -105,22 +50,19 @@ def deleteClass():
 
 @app.route('/students', methods=['GET', 'POST'])
 def students():
-    db.create_all()
-    result2 = Child.query.order_by(Child.c_id)
-    return render_template('studenttable.html', students=result2, )
+    result2 = Child.query.filter_by(Child.c_id)
+    return render_template('students.html', result2=result2)
 
 
 @app.route('/addstudents', methods=['POST', 'GET'])
 def addstudents():
     if request.method == 'POST':
-        students = Child(request.form['c_id'])
+        students = request.form['c_id']
         db.session.add(students)
         db.session.commit()
-        return redirect(url_for('studenttable'))
+        return redirect(url_for('students'))
     else:
         return render_template('addstudent.html', students=students)
-    # else:
-    #     return render_template('addstudent.html', students=students)
 
 
 @app.route('/deletestudent<c_id>', methods=['GET', 'POST'])
@@ -136,7 +78,6 @@ def deletestudent():
     else:
         return redirect(url_for('students', students=students))
 
-# if __name__ == '__main__':
-#     app.run(threaded=True,debug=True)
+
 if __name__ == "__main__":
     app.run(debug=True)
--- model.py
+++ model.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:0143@localhost/db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/db'
 app.config['SECRET_KEY'] = 'secret'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # relationship between specifics and logs
@@ -16,8 +16,7 @@ activity = db.Table('activity',
 report = db.Table('report',
                     db.Column('item_num', db.Integer, db.ForeignKey('items.item_num')),
                     db.Column('prog_num', db.Integer, db.ForeignKey('progress.prog_num'))
-                 )
-
+                  )
 
 class Account(db.Model):
     acc_id = db.Column(db.Integer, primary_key=True)
@@ -35,7 +34,6 @@ class Account(db.Model):
     def __repr__(self):
         return '<Account %r>' % self.acc_type
 
-
 class Access_Token(db.Model):
     tkn_num = db.Column(db.Integer, primary_key=True)
     token = db.Column(db.String(20), unique=True)
@@ -45,7 +43,6 @@ class Access_Token(db.Model):
 
     def __repr__(self):
         return '<Access_Token %r>' % self.token
-
 
 class Parent(db.Model):
     p_id = db.Column(db.Integer, primary_key=True)
@@ -62,16 +59,13 @@ class Parent(db.Model):
 
     def __repr__(self):
         return '<Parent %r>' % self.fname_p
-
-
 class Child(db.Model):
     c_id = db.Column(db.Integer, primary_key=True)
     fname_c = db.Column(db.String(80))
     lname_c = db.Column(db.String(80))
     bday_c = db.Column(db.Date)
     diagnosis = db.Column(db.String(50))
-    pers = db.relationship('Personal', backref='Child', lazy='dynamic')
-    Class = db.relationship('Class', backref='Child', lazy ='dynamic')
+    pers = db.relationship('Personal', backref='child', lazy='dynamic')
 
     def __init__(self, fname_c, lname_c, bday_c, diagnosis):
         self.fname_c = fname_c
@@ -82,7 +76,6 @@ class Child(db.Model):
     def __repr__(self):
         return '<Child %r>' % self.fname_c
 
-
 class Teacher(db.Model):
     t_id = db.Column(db.Integer, primary_key=True)
     fname_t = db.Column(db.String(80))
@@ -91,7 +84,6 @@ class Teacher(db.Model):
     specialty = db.Column(db.String(120))
     tel_num = db.Column(db.BigInteger)
     add_t = db.Column(db.String(120))
-    Class = db.relationship('Class', backref='Teacher', lazy ='dynamic')
 
     def __init__(self, fname_t, lname_t, bday_t, specialty, tel_num, add_t):
         self.fname_t = fname_t
@@ -103,7 +95,6 @@ class Teacher(db.Model):
 
     def __repr__(self):
         return '<Teacher %r>' % self.fname_t
-
 
 class Personal(db.Model):
     per_num = db.Column(db.Integer, primary_key=True)
@@ -117,7 +108,6 @@ class Personal(db.Model):
     def __repr__(self):
         return '<Personal %r>' % self.per_name
 
-
 class Specifics(db.Model):
     spec_num = db.Column(db.Integer, primary_key=True)
     spec_name = db.Column(db.String(50))
@@ -129,7 +119,6 @@ class Specifics(db.Model):
 
     def __repr__(self):
         return '<Specifics %r>' % self.spec_name
-
 
 class Logs(db.Model):
     log_num = db.Column(db.Integer, primary_key=True)
@@ -145,14 +134,10 @@ class Logs(db.Model):
     def __repr__(self):
         return '<Logs %r>' % self.clicks
 
-
 class Class(db.Model):
-    class_id = db.Column(db.Integer, primary_key=True)
+    class_num = db.Column(db.Integer, primary_key=True)
     class_name = db.Column(db.String(50))
     ed = db.relationship('Educational', backref='edu', lazy='dynamic')
-    child_id = db.Column(db.Integer, db.ForeignKey('Child.c_id'))
-    t_id = db.Column(db.Integer, db.ForeignKey('Teacher.t_id'))
-
 
     def __init__(self, class_name):
         self.class_name = class_name
@@ -160,11 +145,10 @@ class Class(db.Model):
     def __repr__(self):
         return '<Class %r>' % self.class_name
 
-
 class Educational(db.Model):
     ed_num = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(50))
-    edu_id = db.Column(db.Integer, db.ForeignKey('Class.class_num'))
+    edu_id = db.Column(db.Integer, db.ForeignKey('class.class_num'))
 
     def __init__(self, subject):
         self.subject = subject
@@ -172,10 +156,9 @@ class Educational(db.Model):
     def __repr__(self):
         return '<Educational %r>' % self.per_subject
 
-
 class Items(db.Model):
     item_num = db.Column(db.Integer, primary_key=True)
-    desc = db.Column(db.String(120)),
+    desc = db.Column(db.String(120))
     rep = db.relationship('Progress', secondary=report, backref=db.backref('rep', lazy='dynamic'))
 
     def __init__(self, desc):
@@ -183,7 +166,6 @@ class Items(db.Model):
 
     def __repr__(self):
         return '<Items %r>' % self.desc
-
 
 class Progress(db.Model):
     prog_num = db.Column(db.Integer, primary_key=True)
@@ -201,7 +183,6 @@ class Progress(db.Model):
     def __repr__(self):
         return '<Progress %r>' % self.details
 
-
 class Images(db.Model):
     img_id = db.Column(db.Integer, primary_key=True)
     img = db.Column(db.String(50))
@@ -211,8 +192,6 @@ class Images(db.Model):
 
     def __repr__(self):
         return '<Images %r>' % self.img
-
-
 class Audio(db.Model):
     aud_id = db.Column(db.Integer, primary_key=True)
     aud = db.Column(db.String(50))
@@ -223,10 +202,8 @@ class Audio(db.Model):
     def __repr__(self):
         return '<Audio %r>' % self.aud
 
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 db.create_all()
-
-
-if __name__ == '__main__':
-    app.run()
-
