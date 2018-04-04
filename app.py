eb827e525b2dda3941dd0781702a6cbb35403b34
ruelbontuyan <ruelbontuyan@yahoo.com>
2018-04-04 13:43:47 +0800

    updated enroll

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def manageclass():
 
 @app.route('/addClass', methods=['POST', 'GET'])
 def addClass():
+
     if request.method == 'POST':
-        
-        classes = Class.query.order_by(Class.class_name).first()
+        classes = Class(Class.class_name)
         classes.class_name = request.form['class_name']
 
 
@@ -71,7 +71,7 @@ def students():
 def addstudents():
     if request.method == 'POST':
 
-        student = Child.query.order_by(Child.c_id).first()
+        student = Child(Child.c_id).first()
         student.c_id = request.form['c_id']
         student.fname_c = request.form['fname_c']
         student.lname_c = request.form['lname_c']
@@ -99,67 +99,6 @@ def deletestudent(c_id):
 
 ################################################################
 
-
-# @app.route('/Class')
-# def Class():
-#     result1 = Class.query.order_by(Class.class_id).all()
-#     return render_template('class.html', result1=result1)
-
-
-# @app.route('/addClass', methods=['POST', 'GET'])
-# def addClass():
-#     if request.method == 'POST':
-#         Class = request.form['class_name']
-#         db.session.add(Class)
-#         db.session.commit()
-#         return redirect(url_for('class', Class=Class))
-#     else:
-#         return render_template('addclass.html')
-
-
-# @app.route('/deleteClass/<class_id>', methods=['GET', 'POST'])
-# def deleteClass():
-#     Class = Class.query.order_by(Class.class_id)
-#     if request.method == 'POST':
-#         Store = request.form['storage']
-#         results = Class.query.filter_by(class_id=Store).first()
-#         db.session.delete(results)
-#         db.session.commit()
-#         Class = Class.query.order_by(Class.class_id)
-#         return redirect(url_for('Class', Class=Class))
-#     else:
-#         return redirect(url_for('Class', Class=Class))
-
-
-# @app.route('/students', methods=['GET', 'POST'])
-# def students():
-#     result2 = Child.query.filter_by(Child.c_id)
-#     return render_template('students.html', result2=result2)
-
-
-# @app.route('/addstudents', methods=['POST', 'GET'])
-# def addstudents():
-#     if request.method == 'POST':
-#         students = request.form['c_id']
-#         db.session.add(students)
-#         db.session.commit()
-#         return redirect(url_for('students'))
-#     else:
-#         return render_template('addstudent.html', students=students)
-
-
-# @app.route('/deletestudent<c_id>', methods=['GET', 'POST'])
-# def deletestudent():
-#     students = Child.query.order_by(Child.c_id)
-#     if request.method == 'POST':
-#         Store = request.form['storage']
-#         result = Child.query.filter_by(c_id=Store).first()
-#         db.session.delete(result)
-#         db.session.commit()
-#         students = Child.query.order_by(students.c_id)
-#         return redirect(url_for('students', students=students))
-#     else:
-#         return redirect(url_for('students', students=students))
 
 
 if __name__ == "__main__":
